b6d3f43242515266e895c1ee868432a0458a88fd
ceveflo <ceraveflomaster@gmail.com>
2017-02-16 03:07:04 -0600

    event log code

---
+++ AGFiles Only/CoreFiles/eventLog.jsx
@@ -0,0 +1,34 @@
+﻿function writeLog(fileFolder, name){
+        
+        this.file  = File(fileFolder+ "/Eventlog_"+name+".txt");
+
+        var file = this.file;                
+        
+        this.createEvent = function(event){
+            var time = new Date();
+
+            var seconds = time.getSeconds();
+            var minutes = time.getMinutes();
+            var hour = time.getHours();
+            var eventString = "["+(time.getYear() + 1900)+"/"+(time.getMonth() + 1)+"/"+time.getDay()+" "+hour+":"+minutes+":"+seconds+"]: " + event;
+                        
+            var prevdata = "";
+            
+            
+            if(file.exists){
+                file.encoding = "UTF8";
+                file.open("e", "TEXT", "????");
+                prevdata = file.read();                
+                $.writeln(prevdata);
+                file.close();
+                //file.remove();
+            }
+        
+            file.encoding = "UTF8";
+            file.open("e", "TEXT", "????");
+            file.writeln(prevdata + eventString);
+            file.close();
+                       
+        }
+
+}
--- AGGenerator.jsx
+++ AGGenerator.jsx
@@ -6,8 +6,12 @@ var psd_files = fileFolder + "/AGFiles Only/psd_files/";
 var temp_files = fileFolder + "/AGFiles Only/temp/";
 
 var toJson = File(coreFolder + "json.jsx");
+var writelog = File(coreFolder + "eventLog.jsx");
 $.evalFile (toJson);
+$.evalFile (writelog);
 
+var AGlog = new writeLog (fileFolder+ "/AGFiles Only", "Generator");
+AGlog.createEvent('Update: Buscando actualizaciones');
 var statusScripts = true;
 
 $.evalFile(updater);
@@ -16,5 +20,6 @@ var up = new Update();
 var statusScripts = up.result;
 
 if(statusScripts){
+    AGlog.createEvent('Update [Result]: Generador actualizado');
     $.evalFile(Main);
 }
